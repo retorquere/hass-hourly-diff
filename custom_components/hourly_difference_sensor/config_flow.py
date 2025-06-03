016753d07bcfc1e4d0f5bb79b49032e44a3a7716
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-06-03 17:14:28 +0200

    config-flow

---
+++ custom_components/hourly_difference_sensor/config_flow.py
@@ -0,0 +1,44 @@
+"""Config flow for Hourly Difference Sensor integration."""
+from __future__ import annotations
+
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+from homeassistant.const import CONF_NAME
+
+DOMAIN = "hourly_difference_sensor"
+CONF_SENSORS = "sensors"
+CONF_SCAN_INTERVAL = "scan_interval_minutes"
+
+class HourlyDifferenceSensorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Hourly Difference Sensor."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            # Validate sensors list
+            sensors = user_input.get(CONF_SENSORS, [])
+            if not sensors:
+                errors[CONF_SENSORS] = "required"
+            else:
+                return self.async_create_entry(
+                    title=user_input.get(CONF_NAME, "Hourly Difference Sensor"),
+                    data=user_input,
+                )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Optional(CONF_NAME, default="Hourly Difference Sensor"): str,
+                vol.Required(CONF_SENSORS): vol.All(
+                    cv.ensure_list, [str]
+                ),
+                vol.Optional(CONF_SCAN_INTERVAL, default=60): vol.All(int, vol.Range(min=1)),
+            }),
+            errors=errors,
+        )
+
+# Helper for list input
+import homeassistant.helpers.config_validation as cv
